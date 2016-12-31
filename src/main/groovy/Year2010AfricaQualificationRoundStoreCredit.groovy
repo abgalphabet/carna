047430336ba3541740a679fb0f7215e867b362c6
arthur <arthur.mw.yeung@gmail.com>
2016-12-31 12:19:55 +0800

    completed 1st problem

---
+++ src/main/groovy/Year2010AfricaQualificationRoundStoreCredit.groovy
@@ -0,0 +1,57 @@
+/**
+ * Created by arthur on 25/12/2016.
+ */
+class Year2010AfricaQualificationRoundStoreCredit {
+    static void main(String[] args) {
+        new Year2010AfricaQualificationRoundStoreCredit(args[0])
+    }
+
+    File run(File infile) {
+        List<Map> specs = parseSpec(infile)
+
+        List<List> solutions = specs.collect { Map test ->
+            List<List> combinations = chooseK(test.noOfItems as int , 2)
+            combinations.find { c -> test.credit == test.priceOfItems[c[0]] + test.priceOfItems[c[1]] }
+        }
+
+        def outfile = new File("${infile.name.split('\\.')[0]}.out")
+        outfile.withWriter('utf-8') { out ->
+            solutions.eachWithIndex { List solution, int i ->
+                List listStartsWtihOne = solution.collect { it + 1 }
+                out.writeLine "Case #${i + 1}: ${listStartsWtihOne.join(' ')}"
+            }
+        }
+
+        outfile
+    }
+
+    private List<Map> parseSpec(File infile) {
+        List<String> ins = infile.readLines()
+        int noOfTests = ins[0] as int
+
+        List specs = []
+        ins[1..-1].eachWithIndex { String entry, int i ->
+            int integral = (i / 3) as int
+            int remainder = i % 3
+
+            switch (remainder) {
+                case 0: specs << [credit: entry as Integer]; break;
+                case 1: specs[integral] << [noOfItems: entry as Integer]; break;
+                default: specs[integral] << [priceOfItems: entry.split(' ').collect { it as int }]; break;
+            }
+        }
+
+        specs
+    }
+
+    private List<List> chooseK(int n, int k) {
+        return chooseK((0..n-1) as int[], 0, n, k)
+    }
+
+    private List<List> chooseK(int[] nums, int start, int n, int k) {
+        if (n == 0 || k == 0) return [[]]
+        if (n == k) return [nums[start..-1]]
+
+        chooseK(nums, start + 1, n - 1, k - 1).collect { [nums[start]] + it } + chooseK(nums, start + 1, n - 1, k)
+    }
+}
---
+++ src/test/groovy/Year_2010_Africa_Qualification_Round.groovy
@@ -0,0 +1,28 @@
+import spock.lang.Specification
+
+import static org.hamcrest.core.IsEqual.equalTo
+import static spock.util.matcher.HamcrestSupport.expect
+
+/**
+ * Created by arthur on 25/12/2016.
+ */
+class Year_2010_Africa_Qualification_Round extends Specification {
+
+    def "load sample store credit file"() {
+        given:
+        String filename = "store_credit_small"
+        URI inUri = this.class.getResource("${filename}.in").toURI()
+        URI outUri = this.class.getResource("${filename}.out").toURI()
+
+
+        when:
+        File outfile = new Year2010AfricaQualificationRoundStoreCredit().run(new File(inUri.path))
+
+
+        then:
+        expect outfile.name, equalTo("${filename}.out" as String)
+        expect outfile.text, equalTo(new File(outUri.path).text + '\n')
+
+    }
+
+}
